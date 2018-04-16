f39be49922d167156dfdbf3dc33ee80b405a57e4
jiangc <jiangc@deepblueai.com>
2018-04-16 11:56:40 +0800

    【新增】增加zuul、config配置

---
+++ demo-config-client/src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @Value("${foo}")
+    private String foo;
+
+    @GetMapping("/hi")
+    public String hi(){
+        return foo;
+    }
+}
